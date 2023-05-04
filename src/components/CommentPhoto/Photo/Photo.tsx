5fd2e4b70a258fe70309c2e39b73f6ec6422cf2f
Gurgen <gsarkisdganyan@gmail.com>
2023-05-04 09:32:55 +0300

    - readme - styles fix

--- src/components/CommentPhoto/Photo/Photo.tsx
+++ src/components/CommentPhoto/Photo/Photo.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import Picture from "../../../shared/ui/Picture/ui/Picture";
 import mockImg from '../../../assets/images/img.png';
+import s from './Photo.module.scss';
 
 type PropsType = {
     image: string;
@@ -10,12 +11,12 @@ const Photo: FC<PropsType> = (props) => {
         image
     } = props;
     return (
-        <>
+        <div className={s.Photo}>
             {image
                 ? <Picture src={image} width={405} height={405} contain alt='picture' radiusMax/>
                 : <Picture src={mockImg} width={405} height={405} contain alt='picture' radiusMax/>
             }
-        </>
+        </div>
     );
 };
 
--- src/components/Header/Content/Avatar/Avatar.tsx
+++ src/components/Header/Content/Avatar/Avatar.tsx
@@ -10,12 +10,15 @@ const Avatar = () => {
 
     return (
         <div className={s.Avatar}>
-            <Picture
-                src={'https://media.istockphoto.com/id/1361586559/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BE%D1%81%D0%B5%D0%BD%D1%8C-%D0%B2-%D0%BB%D0%B5%D1%81%D1%83-%D0%BD%D0%B0-%D0%B7%D0%B0%D0%BA%D0%B0%D1%82%D0%B5.jpg?s=1024x1024&w=is&k=20&c=N4jNVe4n6VbCW50nUuPCVxRuL3LcQYv59qjZ8fsfM7Q='}
-                width={128}
-                height={128}
-                round
-            />
+            <div className={s.avatarPicture}>
+                <Picture
+                    src={'https://media.istockphoto.com/id/1361586559/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BE%D1%81%D0%B5%D0%BD%D1%8C-%D0%B2-%D0%BB%D0%B5%D1%81%D1%83-%D0%BD%D0%B0-%D0%B7%D0%B0%D0%BA%D0%B0%D1%82%D0%B5.jpg?s=1024x1024&w=is&k=20&c=N4jNVe4n6VbCW50nUuPCVxRuL3LcQYv59qjZ8fsfM7Q='}
+                    width={128}
+                    height={128}
+                    round
+                />
+            </div>
+
             <Name setValue={setValue} value={value}/>
         </div>
     );
